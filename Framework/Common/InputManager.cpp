59dbc50d0b2bf865ca58519832d64e3e14a4b2f2
L <zover.v@gmail.com>
2019-01-08 18:04:15 +0800

    add InputManager

---
+++ Framework/Common/InputManager.cpp
@@ -0,0 +1,63 @@
+#include <iostream>
+#include "InputManager.hpp"
+#include "GraphicsManager.hpp"
+#include "geommath.hpp"
+
+using namespace My;
+using namespace std;
+
+int  InputManager::Initialize() { return 0; }
+void InputManager::Finalize() {}
+void InputManager::Tick() {}
+
+void InputManager::UpArrowKeyDown() {
+#ifdef DEBUG
+    cerr << "[InputManager] Up Arrow Key Down!" << endl;
+#endif
+    g_pGraphicsManager->WorldRotateX(PI / 60.0f);
+}
+
+void InputManager::UpArrowKeyUp() {
+#ifdef DEBUG
+    cerr << "[InputManager] Up Arrow Key Up!" << endl;
+#endif
+}
+
+void InputManager::DownArrowKeyDown() {
+#ifdef DEBUG
+    cerr << "[InputManager] Down Arrow Key Down!" << endl;
+#endif
+    g_pGraphicsManager->WorldRotateX(-PI / 60.0f);
+}
+
+void InputManager::DownArrowKeyUp() {
+#ifdef DEBUG
+    cerr << "[InputManager] Down Arrow Key Up!" << endl;
+#endif
+}
+
+void InputManager::LeftArrowKeyDown() {
+#ifdef DEBUG
+    cerr << "[InputManager] Left Arrow Key Down!" << endl;
+#endif
+    g_pGraphicsManager->WorldRotateY(-PI / 60.0f);
+}
+
+void InputManager::LeftArrowKeyUp() {
+#ifdef DEBUG
+    cerr << "[InputManager] Left Arrow Key Up!" << endl;
+#endif
+}
+
+void InputManager::RightArrowKeyDown() {
+#ifdef DEBUG
+    cerr << "[InputManager] Right Arrow Key Down!" << endl;
+#endif
+    g_pGraphicsManager->WorldRotateY(PI / 60.0f);
+}
+
+void InputManager::RightArrowKeyUp() {
+#ifdef DEBUG
+    cerr << "[InputManager] Right Arrow Key Up!" << endl;
+#endif
+}
---
+++ Framework/Common/InputManager.hpp
@@ -0,0 +1,22 @@
+#pragma once
+#include "IRuntimeModule.hpp"
+
+namespace My {
+class InputManager : implements IRuntimeModule {
+public:
+    virtual int  Initialize();
+    virtual void Finalize();
+    virtual void Tick();
+
+    void UpArrowKeyDown();
+    void UpArrowKeyUp();
+    void DownArrowKeyDown();
+    void DownArrowKeyUp();
+    void LeftArrowKeyDown();
+    void LeftArrowKeyUp();
+    void RightArrowKeyDown();
+    void RightArrowKeyUp();
+};
+
+extern InputManager* g_pInputManager;
+}  // namespace My
