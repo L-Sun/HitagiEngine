62d277a468417dc139b5baa56a13b7ae0e0b0e3c
L <zover.v@gmail.com>
2019-01-14 16:08:44 +0800

    fix fragment shader error

--- Asset/Shaders/basic.fs
+++ Asset/Shaders/basic.fs
@@ -24,10 +24,10 @@ void main(void) {
 
     vec3 N = normalize(normal.xyz);
     vec3 L = normalize((viewMatrix * vec4(lightPosition, 1.0f)).xyz - v.xyz);
-    vec3 R = normalize(2 * dot(L,N) * N - L);
+    vec3 R = normalize(2 * clamp(dot(L,N), 0.0f, 1.0f) * N - L);
     vec3 V = normalize(v.xyz);
     if (diffuseColor.r < 0)
-        outputColor = vec4((ambientColor.rgb + lightColor.rgb * dot(N, L) + specularColor.rgb * pow(clamp(dot(R, V), 0.0f, 1.0f), specularPower))* texture(defaultSampler, uv).rgb, 1.0f); 
+        outputColor = vec4(ambientColor.rgb + lightColor.rgb* texture(defaultSampler, uv).rgb * clamp(dot(N, L),0.0f, 1.0f) + specularColor.rgb * pow(clamp(dot(R, V), 0.0f, 1.0f), specularPower), 1.0f); 
     else
-        outputColor = vec4((ambientColor.rgb + lightColor.rgb * dot(N, L) + specularColor.rgb * pow(clamp(dot(R,V), 0.0f, 1.0f), specularPower))* diffuseColor.rgb, 1.0f);  
+        outputColor = vec4(ambientColor.rgb + lightColor.rgb * diffuseColor.rgb * clamp(dot(N, L),0.0f, 1.0f) + specularColor.rgb * pow(clamp(dot(R,V), 0.0f, 1.0f), specularPower), 1.0f);  
 }
