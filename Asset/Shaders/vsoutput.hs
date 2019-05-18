85e7b0a8634353237ee1ccf87f02a57a0ab74064
L <zover.v@gmail.com>
2019-05-18 11:09:24 +0800

    add dx12 shader

---
+++ Asset/Shaders/vsoutput.hs
@@ -0,0 +1,14 @@
+#ifndef __VSOUTPUT_H__
+#define __VSOUTPUT_H__
+
+struct v2p
+{
+    float4 Position     : SV_POSITION;
+    float2 TextureUV    : TEXCOORD0;
+	float3 vNorm		: TEXCOORD1;
+	float3 vPosInView	: TEXCOORD3;
+};
+
+
+
+#endif
