9761da2098c2258a020dd920a987ca38508cc99d
L <zover.v@gmail.com>
2020-05-23 13:29:44 +0800

    fix buffer allocate error

--- Hitagi/Core/Buffer.cpp
+++ Hitagi/Core/Buffer.cpp
@@ -6,7 +6,6 @@
 
 namespace Hitagi::Core {
 
-Buffer::Buffer() :  m_Alignment(alignof(uint32_t)) {}
 Buffer::Buffer(size_t size, size_t alignment)
     : m_Size(size),
       m_Alignment(alignment) {
@@ -20,16 +19,16 @@ Buffer::Buffer(const void* initialData, size_t size, size_t alignment)
     std::memcpy(m_Data, initialData, m_Size);
 }
 
-Buffer::Buffer(const Buffer& buffer) {
-    m_Data = reinterpret_cast<uint8_t*>(g_MemoryManager->Allocate(buffer.m_Size, m_Alignment));
+Buffer::Buffer(const Buffer& buffer)
+    : m_Data(reinterpret_cast<uint8_t*>(g_MemoryManager->Allocate(buffer.m_Size, buffer.m_Alignment))),
+      m_Size(buffer.m_Size),
+      m_Alignment(buffer.m_Alignment) {
     std::memcpy(m_Data, buffer.m_Data, buffer.m_Size);
-    m_Size      = buffer.m_Size;
-    m_Alignment = buffer.m_Alignment;
 }
 Buffer::Buffer(Buffer&& buffer) : m_Data(buffer.m_Data), m_Size(buffer.m_Size), m_Alignment(buffer.m_Alignment) {
     buffer.m_Data      = nullptr;
     buffer.m_Size      = 0;
-    buffer.m_Alignment = 4;
+    buffer.m_Alignment = 0;
 }
 Buffer& Buffer::operator=(const Buffer& rhs) {
     if (this != &rhs) {
@@ -53,7 +52,7 @@ Buffer& Buffer::operator=(Buffer&& rhs) {
         m_Alignment     = rhs.m_Alignment;
         rhs.m_Data      = nullptr;
         rhs.m_Size      = 0;
-        rhs.m_Alignment = 4;
+        rhs.m_Alignment = 0;
     }
     return *this;
 }
@@ -62,7 +61,13 @@ Buffer::~Buffer() {
     m_Data = nullptr;
 }
 
-uint8_t*       Buffer::GetData() { return m_Data; }
-const uint8_t* Buffer::GetData() const { return m_Data; }
-size_t         Buffer::GetDataSize() const { return m_Size; }
+uint8_t* Buffer::GetData() {
+    return m_Data;
+}
+const uint8_t* Buffer::GetData() const {
+    return m_Data;
+}
+size_t Buffer::GetDataSize() const {
+    return m_Size;
+}
 }  // namespace Hitagi::Core
--- Hitagi/Core/Buffer.hpp
+++ Hitagi/Core/Buffer.hpp
@@ -5,7 +5,7 @@ namespace Hitagi::Core {
 
 class Buffer {
 public:
-    Buffer();
+    Buffer() = default;
     Buffer(size_t size, size_t alignment = 4);
     Buffer(const void* initialData, size_t size, size_t alignment = 4);
     Buffer(const Buffer& buffer);
@@ -18,9 +18,11 @@ public:
     const uint8_t* GetData() const;
     size_t         GetDataSize() const;
 
+    operator bool() const { return m_Data == nullptr || m_Size == 0; }
+
 private:
-    uint8_t* m_Data = nullptr;
-    size_t   m_Size = 0;
-    size_t   m_Alignment;
+    uint8_t* m_Data      = nullptr;
+    size_t   m_Size      = 0;
+    size_t   m_Alignment = 0;
 };
 }  // namespace Hitagi::Core
