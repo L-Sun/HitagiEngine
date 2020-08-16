fc479aa65df5085bbeba1a3eb0e1c37cd74ce300
L <zover.v@gmail.com>
2020-08-16 14:47:26 +0800

    fix shader error on d3d feature 11_1

--- Hitagi/Graphics/DX12/DX12DriverAPI.cpp
+++ Hitagi/Graphics/DX12/DX12DriverAPI.cpp
@@ -28,7 +28,7 @@ DX12DriverAPI::DX12DriverAPI() : DriverAPI(APIType::DirectX12) {
 
     // Create device.
     {
-        if (FAILED(D3D12CreateDevice(nullptr, D3D_FEATURE_LEVEL_12_0, IID_PPV_ARGS(&m_Device)))) {
+        if (FAILED(D3D12CreateDevice(nullptr, D3D_FEATURE_LEVEL_11_1, IID_PPV_ARGS(&m_Device)))) {
             ComPtr<IDXGIAdapter4> pWarpaAdapter;
             ThrowIfFailed(m_DxgiFactory->EnumWarpAdapter(IID_PPV_ARGS(&pWarpaAdapter)));
             ThrowIfFailed(D3D12CreateDevice(pWarpaAdapter.Get(), D3D_FEATURE_LEVEL_12_0, IID_PPV_ARGS(&m_Device)));
