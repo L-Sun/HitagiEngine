a24a1f4aa95efde30e0889d94a6aa162c4da89c0
L <zover.v@gmail.com>
2022-01-29 20:23:44 +0800

    🚧 pmr

--- Hitagi/Core/MemoryManager.cpp
+++ Hitagi/Core/MemoryManager.cpp
@@ -13,14 +13,83 @@ std::unique_ptr<Core::MemoryManager> g_MemoryManager = std::make_unique<Core::Me
 
 namespace Hitagi::Core {
 
-void* MemoryPool::do_allocate(size_t bytes, size_t alignment) {
-    m_Logger->debug("Allocate {} byte(s), align: {} byte(s)", bytes, alignment);
-    return synchronized_pool_resource::do_allocate(bytes, alignment);
+auto MemoryPool::Pool::new_page() -> Page& {
+    auto page = std::unique_ptr<std::byte[]>(
+        new (std::align_val_t(block_size)) std::byte[page_size]);
+
+    size_t num_block = page_size / block_size;
+    num_free_blocks += num_block;
+
+    constexpr auto next_block = [](Block* block, size_t block_size) -> Block* {
+        return reinterpret_cast<Block*>(reinterpret_cast<std::byte*>(block) + block_size);
+    };
+
+    auto p_block = reinterpret_cast<Block*>(page.get());
+    for (size_t i = 0; i < num_block - 1; i++) {
+        p_block->next = next_block(p_block, block_size);
+        p_block       = next_block(p_block, block_size);
+    }
+    p_block->next = nullptr;
+    return pages.emplace_back(std::move(page));
 }
 
-void MemoryPool::do_deallocate(void* p, size_t bytes, size_t alignment) {
-    m_Logger->debug("Deallocate {} byte(s), align: {} byte(s)", bytes, alignment);
-    synchronized_pool_resource::do_deallocate(p, bytes, alignment);
+auto MemoryPool::Pool::allocate() -> Block* {
+    std::lock_guard lock(m_Mutex);
+
+    if (free_list == nullptr) {
+        auto& page        = new_page();
+        auto  first_block = reinterpret_cast<Block*>(page.get());
+        free_list         = first_block;
+    }
+    Block* result = free_list;
+    free_list     = free_list->next;
+    num_free_blocks--;
+    return result;
+}
+
+auto MemoryPool::Pool::deallocate(Block* block) -> void {
+    std::lock_guard lock(m_Mutex);
+
+    block->next = free_list;
+    free_list   = block;
+}
+
+MemoryPool::MemoryPool(std::shared_ptr<spdlog::logger> logger)
+    : m_Logger(logger) {
+    m_Logger->set_level(spdlog::level::debug);
+    for (size_t i = 0; i < block_size.size(); i++) {
+        m_Pools.at(i).block_size = block_size.at(i);
+        m_Pools.at(i).page_size  = 8_kB;
+    }
+}
+
+auto MemoryPool::GetPool(size_t bytes) -> std::optional<std::reference_wrapper<Pool>> {
+    auto   iter  = std::lower_bound(std::begin(block_size), std::end(block_size), bytes);
+    size_t index = std::distance(std::begin(block_size), iter);
+    if (index == m_Pools.size()) {
+        return std::nullopt;
+    }
+    return m_Pools.at(index);
+}
+
+auto MemoryPool::do_allocate(size_t bytes, size_t alignment) -> void* {
+    if (auto pool = GetPool(bytes); pool.has_value()) {
+        auto result = pool->get().allocate();
+        m_Logger->debug("Pool({} bytes): num_pages: {}, free_blocks: {}",
+                        pool->get().block_size,
+                        pool->get().pages.size(),
+                        pool->get().num_free_blocks);
+        return result;
+    }
+
+    m_Logger->debug("using new/delete to allocate {} bytes", bytes);
+    return new (std::align_val_t(alignment)) std::byte[bytes];
+}
+
+auto MemoryPool::do_deallocate(void* p, size_t bytes, size_t alignment) -> void {
+    if (auto pool = GetPool(bytes); pool.has_value()) return pool->get().deallocate(reinterpret_cast<Block*>(p));
+
+    delete[] reinterpret_cast<std::byte*>(p);
 }
 
 int MemoryManager::Initialize() {
@@ -28,11 +97,7 @@ int MemoryManager::Initialize() {
     m_Logger->info("Initialize...");
 
     m_Logger->info("Initial Memory Pool...");
-    m_Pools = std::make_unique<MemoryPool>(
-        std::pmr::pool_options{
-            .max_blocks_per_chunk        = 4096,
-            .largest_required_pool_block = 8_kb},
-        m_Logger);
+    m_Pools = std::make_unique<MemoryPool>(m_Logger);
 
     m_Logger->debug("Set pmr default resource");
     std::pmr::set_default_resource(m_Pools.get());
--- Hitagi/Core/MemoryManager.hpp
+++ Hitagi/Core/MemoryManager.hpp
@@ -1,22 +1,62 @@
 #pragma once
 #include "IRuntimeModule.hpp"
 
+#include <list>
+#include <array>
 #include <memory_resource>
 
 namespace Hitagi::Core {
-constexpr std::uint64_t operator""_kb(unsigned long long val) { return static_cast<std::uint64_t>(val) << 10; }
 
-class MemoryPool : public std::pmr::synchronized_pool_resource {
+constexpr size_t operator""_kB(unsigned long long val) { return val << 10; }
+
+class MemoryPool : public std::pmr::memory_resource {
 public:
-    MemoryPool(const std::pmr::pool_options& options, std::shared_ptr<spdlog::logger> logger)
-        : synchronized_pool_resource(options),
-          m_Logger(logger) {}
+    MemoryPool(std::shared_ptr<spdlog::logger> logger);
+    MemoryPool(const MemoryPool&) = delete;
+    MemoryPool& operator=(const MemoryPool&) = delete;
 
 private:
     [[nodiscard]] void* do_allocate(size_t bytes, size_t alignment = alignof(std::max_align_t)) final;
     void                do_deallocate(void* p, size_t bytes, size_t alignment) final;
+    bool                do_is_equal(const std::pmr::memory_resource& other) const noexcept final {
+        return this == &other;
+    }
 
-    std::shared_ptr<spdlog::logger> m_Logger = nullptr;
+    struct Block {
+        Block* next = nullptr;
+    };
+
+    using Page = std::unique_ptr<std::byte[]>;
+
+    struct Pool {
+        std::mutex m_Mutex;
+
+        std::list<Page> pages;
+        Block*          free_list = nullptr;
+        size_t          page_size;
+        size_t          block_size;
+        size_t          num_free_blocks = 0;
+
+        Page&                new_page();
+        [[nodiscard]] Block* allocate();
+        void                 deallocate(Block* block);
+    };
+
+    std::optional<std::reference_wrapper<Pool>> GetPool(size_t bytes);
+
+    constexpr static std::array block_size = {
+        // 4-increments
+        8, 12, 16, 20, 24, 28, 32, 36, 40, 44, 48, 52, 56, 60, 64, 68, 72, 76, 80, 84, 88, 92, 96,
+
+        // 32-increments
+        128, 160, 192, 224, 256, 288, 320, 352, 384, 416, 448, 480, 512, 544, 576, 608, 640,
+
+        // 64-increments
+        704, 768, 832, 896, 960, 1024};
+
+    std::array<Pool, block_size.size()> m_Pools;
+
+    std::shared_ptr<spdlog::logger> m_Logger;
 };
 
 class MemoryManager : public IRuntimeModule {
