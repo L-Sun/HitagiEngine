184a520b5ce28035ed264f67f954bfedacafefdc
L <zover.v@gmail.com>
2022-03-13 15:21:16 +0800

    🔥 clear dnn files

--- Examples/Game1/MyGame.hpp
+++ Examples/Game1/MyGame.hpp
@@ -1,7 +1,4 @@
 #pragma once
-#include "MANN.hpp"
-#include "DLCharacter.hpp"
-
 #include "GameLogic.hpp"
 #include "Editor.hpp"
 
@@ -11,18 +8,7 @@ public:
     void Finalize() final;
     void Tick() final;
 
-    void DrawBone();
-    void DumpAnimation();
-    void SwitchDL();
-
 private:
-    void UpdateVelocities();
-
     Hitagi::Core::Clock m_Clock;
-    bool                m_ShowEditor = true;
     Hitagi::Editor      m_Editor;
-
-    std::unique_ptr<MANN> m_MANN;
-
-    std::unordered_map<std::shared_ptr<Hitagi::Asset::SceneNode>, std::shared_ptr<DLCharacter>> m_DLCharacters;
 };
