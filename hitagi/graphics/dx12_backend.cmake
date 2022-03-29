dd91f2491b6b1c2466bf5540861a41fd61923e40
L <zover.v@gmail.com>
2022-03-29 20:37:45 +0800

    🎨 format cmake config

--- hitagi/graphics/dx12_backend.cmake
+++ hitagi/graphics/dx12_backend.cmake
@@ -1,5 +1,4 @@
-add_library(
-    dx12_driver_api
+add_library(dx12_driver_api
     src/backend/dx12/driver_api.cpp
     src/backend/dx12/command_allocator_pool.cpp
     src/backend/dx12/command_list_manager.cpp
@@ -12,11 +11,12 @@ add_library(
     src/backend/dx12/pso.cpp
     src/backend/dx12/dynamic_descriptor_heap.cpp
 )
-target_link_libraries(
-    dx12_driver_api
-    PUBLIC graphics_api
-    PRIVATE math
-            d3d12
-            dxgi
-            dxguid
+target_link_libraries(dx12_driver_api
+    PUBLIC
+        graphics_api
+    PRIVATE
+        math
+        d3d12
+        dxgi
+        dxguid
 )
