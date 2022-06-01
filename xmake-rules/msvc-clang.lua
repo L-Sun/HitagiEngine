d5d0cfb8bdcd998b05b087937fef1bed4842514a
L-Sun <zover.v@gmail.com>
2022-06-01 20:10:42 +0800

    💚 update msvc clang rule

--- xmake-rules/msvc-clang.lua
+++ xmake-rules/msvc-clang.lua
@@ -1,14 +1,19 @@
 rule("clang-msvc")
     on_config(function (target)
         import("core.project.config")
-        local compiler = config.get("cxx")
+        
 
-        if target:is_plat("windows") and compiler == "clang++" then
-            if is_mode("debug") then
-                target:add("defines", "_Debug")
+        if target:is_plat("windows") then
+            local compiler = config.get("cxx")
+            
+            if compiler == "clang++" and is_mode("debug") then
                 target:add("syslinks", "libcmtd")
             else
                 target:add("syslinks", "libcmt")
+            end
+
+            if compiler == "clang" then
+                target:add("cxxflags", "/EHa")
             end
         end
     end)
