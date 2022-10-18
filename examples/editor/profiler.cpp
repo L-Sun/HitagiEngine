697a6b210b73c3b826f956022e886c3abf5d012d
L-Sun <zover.v@gmail.com>
2022-10-18 20:48:10 +0800

    ✨ a tinny time profiler

--- examples/editor/profiler.cpp
+++ examples/editor/profiler.cpp
@@ -29,29 +29,50 @@ void Profiler::Tick() {
             ImGui::Separator();
             // Module time cost
             {
-                std::function<void(const RuntimeModule*)> module_inspector = [&](const RuntimeModule* module) {
-                    if (ImGui::TreeNode(module->GetName().data())) {
-                        {
-                            auto   time_cost = std::chrono::duration_cast<std::chrono::milliseconds>(module->GetProfileTime());
-                            ImVec4 color;
-                            if (time_cost >= 10ms) {
-                                color = ImVec4(1, 0, 0, 1);
-                            } else if (1ms <= time_cost && time_cost < 10ms) {
-                                color = ImVec4(0.95f, 0.77f, 0.06f, 1.0f);
-                            } else {
-                                color = ImVec4(0.53f, 1.0f, 0.29f, 1.0f);
+                static ImGuiTableFlags flags           = ImGuiTableFlags_BordersV | ImGuiTableFlags_BordersOuterH | ImGuiTableFlags_Resizable | ImGuiTableFlags_RowBg | ImGuiTableFlags_NoBordersInBody;
+                static float           TEXT_BASE_WIDTH = ImGui::CalcTextSize("A").x;
+
+                if (ImGui::BeginTable("Module inspector", 2, flags)) {
+                    ImGui::TableSetupColumn("Name", ImGuiTableColumnFlags_NoHide);
+                    ImGui::TableSetupColumn("Time cost", ImGuiTableColumnFlags_WidthFixed, TEXT_BASE_WIDTH * 4.0f);
+                    ImGui::TableHeadersRow();
+
+                    auto time_cost_text = [](const RuntimeModule* module) {
+                        auto   time_cost = std::chrono::duration_cast<std::chrono::milliseconds>(module->GetProfileTime());
+                        ImVec4 color;
+                        if (time_cost >= 10ms) {
+                            color = ImVec4(1, 0, 0, 1);
+                        } else if (1ms <= time_cost && time_cost < 10ms) {
+                            color = ImVec4(0.95f, 0.77f, 0.06f, 1.0f);
+                        } else {
+                            color = ImVec4(0.53f, 1.0f, 0.29f, 1.0f);
+                        }
+                        ImGui::TextColored(color, "%s", fmt::format("{}", time_cost).c_str());
+                    };
+
+                    std::function<void(const RuntimeModule*)> module_inspector = [&](const RuntimeModule* module) {
+                        ImGui::TableNextRow();
+
+                        ImGui::TableSetColumnIndex(1);
+                        time_cost_text(module);
+
+                        auto sub_modules = module->GetSubModules();
+                        ImGui::TableSetColumnIndex(0);
+                        if (!sub_modules.empty()) {
+                            if (ImGui::TreeNodeEx(module->GetName().data(), ImGuiTreeNodeFlags_SpanFullWidth)) {
+                                for (const auto& sub_module : module->GetSubModules()) {
+                                    module_inspector(sub_module);
+                                }
+                                ImGui::TreePop();
                             }
-                            ImGui::Text("Time cost: ");
-                            ImGui::SameLine();
-                            ImGui::TextColored(color, "%s", fmt::format("{}", time_cost).c_str());
+                        } else {
+                            ImGui::TreeNodeEx(module->GetName().data(), ImGuiTreeNodeFlags_Leaf | ImGuiTreeNodeFlags_Bullet | ImGuiTreeNodeFlags_NoTreePushOnOpen | ImGuiTreeNodeFlags_SpanFullWidth);
                         }
-                        for (const auto& sub_module : module->GetSubModules()) {
-                            module_inspector(sub_module);
-                        }
-                        ImGui::TreePop();
-                    }
-                };
-                module_inspector(engine);
+                    };
+
+                    module_inspector(engine);
+                    ImGui::EndTable();
+                }
             }
         }
         ImGui::End();
